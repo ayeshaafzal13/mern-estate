25a968deeefaba4d25de6096bf4a893b678458ce
ayeshaafzal13 <ayeshaafzal67993@gmail.com>
2026-09-23 19:40:44 +0500

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,16 @@
+import dns from "dns";
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to Mongodb Successfully")
+}).catch((err) => {
+    console.log(err);
+});
 
 const app = express();
 
