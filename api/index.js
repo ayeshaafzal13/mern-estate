783a57b93cec19e0bbd40162b3094b292a4483a8
ayeshaafzal13 <ayeshaafzal67993@gmail.com>
2026-09-23 20:26:21 +0500

    Create user model and a test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import dns from "dns";
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
@@ -16,5 +17,6 @@ const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-}
-);
+});
+
+app.use('/api/user', userRouter);
