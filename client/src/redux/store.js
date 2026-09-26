5273bd62aa2bbfddaa6e086a94abfec70fb4aa00
ayeshaafzal13 <ayeshaafzal67993@gmail.com>
2026-09-26 11:24:23 +0500

    Add redux toolkit and persist

---
+++ client/src/redux/store.js
@@ -0,0 +1,31 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice.js';
+import { persistReducer, persistStore } from 'redux-persist';
+
+const storage = {
+    getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+    setItem: (key, value) => Promise.resolve(localStorage.setItem(key, value)),
+    removeItem: (key) => Promise.resolve(localStorage.removeItem(key)),
+};
+
+
+const rootReducer = combineReducers({ user: userReducer});
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
+});
+
+export const persistor = persistStore(store);
